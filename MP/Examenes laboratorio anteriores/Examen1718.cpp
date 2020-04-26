78d02f5c934a49305fb4fe4a894ed01f6e6d8767
mariogmarq <mariogarciamarq@gmail.com>
2020-04-26 10:41:49 +0200

    Correccion examen laboratorio 1718

---
+++ MP/Examenes laboratorio anteriores/Examen1718.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+bool contenidoen(string s, string* c, int tam){
+    for(int i = 0; i < tam; i ++){
+        if(s==c[i])
+            return true;
+    }
+    return false;
+}
+
+void Errorbreak(string error) {
+    cerr << error;
+    exit(1);
+}
+
+void Leer(string ifilename, string* &array1, int &tam1, string* &array, int &tam2){
+    ifstream file;
+    file.open(ifilename);
+    if(!file)
+        Errorbreak("Error al abrir "+ ifilename);
+    file >> tam1;
+    if((file.eof() && tam1 != 0)||tam1 < 0)
+        Errorbreak("Error al leer");
+    array1 = new string[tam1];
+    for(int i = 0; i < tam1; i++){
+        if(!file)
+            Errorbreak("Error al leer");
+        else{
+            file >> array1[i];
+        }
+    }
+    file >> tam2;
+    if((file.eof() && tam2 != 0)||tam2 < 0)
+        Errorbreak("Error al leer");
+    array2 = new string[tam2];
+    for(int i = 0; i < tam2; i++){
+        if(!file)
+            Errorbreak("Error al leer");
+        else{
+            file >> array2[i];
+        }
+    }
+    file.close();
+}
+
+void aumentar(string* &array, int &tam, const string &palabra) {
+    string* newarray = new string[tam+1];
+    for(int i = 0; i < tam; i++){
+        newarray[i] = array[i];
+    }
+    newarray[tam] = palabra;
+    delete[]array;
+    array = newarray;
+    tam++;
+}
+
+int main() {
+    int tam1, tam2, tam3 = 0;
+    string *array1=nullptr, *array2=nullptr, *array3=nullptr;
+    Leer("fichero", array1, tam1, array2, tam2);
+
+    for(int i = 0; i < tam1; i++){
+        if(!contenidoen(array1[i], array2, tam2))
+            aumentar(array3, tam3, array1[i]);
+    }
+    for(int i = 0; i < tam3; i++){
+        cout << array3[i] << " ";
+    }
+    delete[] array1;
+    delete[] array2;
+    delete[] array3;
+    return 0;
+}
